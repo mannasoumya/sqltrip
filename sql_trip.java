74da30a5f6af3cd5553fb037dc23cb5277230cdd
Manna Soumya <sm323265@gmail.com>
2018-03-19 12:33:19 +0530

    Update sql_trip.java

--- sql_trip.java
+++ sql_trip.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.net.*;
-class sql_trip_withconsolecolor extends ConsoleColors
+class sql_trip extends ConsoleColors
 {
 public static void main(String [] args)throws Exception
 {
